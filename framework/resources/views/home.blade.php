592144041518dd937ef521e8e721d1d1948d7b24
Irwin <irwinsharma05@gmail.com>
2021-01-25 04:05:47 +0800

    removed pricing basic and pro

--- framework/resources/views/home.blade.php
+++ framework/resources/views/home.blade.php
@@ -100,59 +100,7 @@
                     </div> <!-- single pricing -->
                 </div>     
                          
-                <div class="col-lg-4 col-md-7 col-sm-9">
-                    <div class="pricing mt-40">
-                        <div class="pricing-baloon">
-                            <img src="assets/images/baloon.svg" alt="baloon">
-                        </div>
-                        <div class="pricing-header">
-                            <h5 class="sub-title">Basic <span class="badge badge-secondary">Coming soon</span></h5>
-                            <span class="price">RM 10</span>
-                            <p class="year">per month</p>
-                        </div>
-                        <div class="pricing-list">
-                            <ul>
-                                <li><i class="lni-check-mark-circle"></i> Up to 300 links</li>
-                                <li><i class="lni-check-mark-circle"></i> Basic analytics</li>
-                                <li><i class="lni-cross-circle"></i> Product landing page</li>
-                                <li><i class="lni-cross-circle"></i> Sub-links</li>
-                            </ul>
-                        </div>
-                        <div class="pricing-btn rounded-buttons text-center">
-                            <a class="main-btn rounded-one" href="#">GET STARTED</a>
-                        </div>
-                        <div class="bottom-shape">
-                            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 112.35"><defs><style>.color-2{fill:#0067f4;isolation:isolate;}.cls-1{opacity:0.1;}.cls-2{opacity:0.2;}.cls-3{opacity:0.4;}.cls-4{opacity:0.6;}</style></defs><title>bottom-part1</title><g><g data-name="Group 747"><path data-name="Path 294" class="cls-1 color-2" d="M0,24.21c120-55.74,214.32,2.57,267,0S349.18,7.4,349.18,7.4V82.35H0Z" transform="translate(0 0)"/><path data-name="Path 297" class="cls-2 color-2" d="M350,34.21c-120-55.74-214.32,2.57-267,0S.82,17.4.82,17.4V92.35H350Z" transform="translate(0 0)"/><path data-name="Path 296" class="cls-3 color-2" d="M0,44.21c120-55.74,214.32,2.57,267,0S349.18,27.4,349.18,27.4v74.95H0Z" transform="translate(0 0)"/><path data-name="Path 295" class="cls-4 color-2" d="M349.17,54.21c-120-55.74-214.32,2.57-267,0S0,37.4,0,37.4v74.95H349.17Z" transform="translate(0 0)"/></g></g></svg>
-                        </div>
-                    </div> <!-- single pricing -->
-                </div>    
-                          
-                <div class="col-lg-4 col-md-7 col-sm-9">
-                    <div class="pricing mt-40">
-                        <div class="pricing-baloon">
-                            <img src="assets/images/baloon.svg" alt="baloon">
-                        </div>
-                        <div class="pricing-header">
-                            <h5 class="sub-title">Pro <span class="badge badge-secondary">Coming soon</span></h5>
-                            <span class="price">RM 35</span>
-                            <p class="year">per month</p>
-                        </div>
-                        <div class="pricing-list">
-                            <ul>
-                                <li><i class="lni-check-mark-circle"></i> Unlimited links</li>
-                                <li><i class="lni-check-mark-circle"></i> Pro analytics</li>
-                                <li><i class="lni-check-mark-circle"></i> Product landing page<span class="badge badge-secondary">Coming soon</span></li>
-                                <li><i class="lni-check-mark-circle"></i> Sub-links<span class="badge badge-secondary">Coming soon</span></li>
-                            </ul>
-                        </div>
-                        <div class="pricing-btn rounded-buttons text-center">
-                            <a class="main-btn rounded-one" href="{{ route('admin.links.create') }}">GET STARTED</a>
-                        </div>
-                        <div class="bottom-shape">
-                            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 112.35"><defs><style>.color-2{fill:#0067f4;isolation:isolate;}.cls-1{opacity:0.1;}.cls-2{opacity:0.2;}.cls-3{opacity:0.4;}.cls-4{opacity:0.6;}</style></defs><title>bottom-part1</title><g><g data-name="Group 747"><path data-name="Path 294" class="cls-1 color-2" d="M0,24.21c120-55.74,214.32,2.57,267,0S349.18,7.4,349.18,7.4V82.35H0Z" transform="translate(0 0)"/><path data-name="Path 297" class="cls-2 color-2" d="M350,34.21c-120-55.74-214.32,2.57-267,0S.82,17.4.82,17.4V92.35H350Z" transform="translate(0 0)"/><path data-name="Path 296" class="cls-3 color-2" d="M0,44.21c120-55.74,214.32,2.57,267,0S349.18,27.4,349.18,27.4v74.95H0Z" transform="translate(0 0)"/><path data-name="Path 295" class="cls-4 color-2" d="M349.17,54.21c-120-55.74-214.32,2.57-267,0S0,37.4,0,37.4v74.95H349.17Z" transform="translate(0 0)"/></g></g></svg>
-                        </div>
-                    </div> <!-- single pricing -->
-                </div>
+                
             </div> <!-- row -->
         </div> <!-- container -->
     </section>
